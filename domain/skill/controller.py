5a360d4243a19cac5183b6258d85cebc53f5a695
Norman Th Glaves <norman.glaves@opencasa.com>
2024-09-03 14:33:19 -0400

    task: skill basics things
    - domain
    - repository

---
+++ domain/skill/controller.py
@@ -0,0 +1,17 @@
+from flask import request
+from repositories import create_uow
+
+from .skill_dto import SkillListDTO, SkillDTO
+
+def find_skill():
+    with create_uow() as uow:
+        skills = uow.skill_repository.find()
+        list = SkillListDTO(skills)
+        return {"status": True, "skills": list.to_json}
+
+def create_skill():
+    payload = SkillDTO(request.json)
+    with create_uow() as uow:
+        #TODO: verify if the skill is there.
+        nw_skill = uow.skill_repository.create(payload)
+        return {"status": True, "payload": SkillDTO(dict(nw_skill)).to_json}
---
+++ domain/skill/skill_dto.py
@@ -0,0 +1,19 @@
+class SkillDTO:
+    def __init__(self, data):
+        self.id = data.get("id")
+        self.skill_name = data.get("skill_name")
+
+    @property
+    def to_json(self):
+        return {
+            "id": self.id,
+            "skill_name": self.skill_name    
+        }
+    
+class SkillListDTO:
+    def __init__(self, data):
+        self.list = [SkillDTO(dict(item)) for item in data]
+
+    @property
+    def to_json(self):
+        return [item.to_json for item in self.list]
