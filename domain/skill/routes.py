4bd215efd386db9cc7733c4719d7e690eecb5333
Norman Th Glaves <norman.glaves@opencasa.com>
2024-09-04 21:08:15 -0400

    task: upload csv for skills

--- domain/skill/routes.py
+++ domain/skill/routes.py
@@ -6,5 +6,6 @@ def add_routes(parent: Blueprint) -> None:
 
     router.add_url_rule('/skill', view_func=controller.find_skill)
     router.add_url_rule('/skill', view_func=controller.create_skill, methods=["POST"])
-    
+    router.add_url_rule("/skill/csv", view_func=controller.upload_csv, methods=["GET", "POST"])
+
     parent.register_blueprint(router)
